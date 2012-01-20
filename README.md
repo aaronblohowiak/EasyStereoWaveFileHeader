ff9450712a51685bd9d9ebb500a97b9f079f5b39
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2012-01-20 12:39:40 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Easy Stereo Wave-File Header.
+## The easiest way to write audio files.
+
+* Trivial to use WAV-file output!
+* Created by Aaron Blohowiak on 11/15/11
+* Based on the documentation from here: https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+
+This library makes writing out stereo wave files very easy by populating the header with nice defaults.
+
+I assume you are on a little-endian platform with 8-bit `char`, like x86/64 or ARM. (Works on iPhone.)
+
+Here is an easy example of how to use this to write some uint16_t audio samples to disk:
+
+    int16_t left_channel[NUMBER_OF_SAMPLES]; //some mono samples as signed integers;
+    int16_t right_channel[NUMBER_OF_SAMPLES]; //some mono samples as signed integers;
+
+    FILE * file;
+    file = fopen("output.wav", "w");
+    waveFormatHeader_t * wh = stereo16bit44khzWaveHeaderForLength(NUMBER_OF_SAMPLES);
+    writeWaveHeaderToFile(wh, file);
+    free(wh);
+
+    for(int i=0;i<NUMBER_OF_SAMPLES;i++){
+    //write the same data to both channels if you have a mono source.
+    //you could make a mono file, but this is just easier =)
+    fwrite(left_channel[i], sizeof(int16_t), 1, file); //left channel
+    fwrite(right_channel[i], sizeof(int16_t), 1, file); //right channel
+    }
+
+    fclose(file);
+
---
+++ wave_header.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "wave_header.h"
+
+waveFormatHeader_t * basicHeader(){
+    waveFormatHeader_t * wh = malloc(sizeof(waveFormatHeader_t));
+    memcpy(wh->ChunkId, &"RIFF", 4);
+    memcpy(wh->Format, &"WAVE", 4);
+    memcpy(wh->Subchunk1ID, &"fmt ", 4);
+    wh -> Subchunk1Size = 16;
+    
+    return wh;
+}
+
+waveFormatHeader_t * stereo16bit44khzWaveHeader(){
+    waveFormatHeader_t * wh = basicHeader();
+    
+    wh->AudioFormat = 1;
+    wh->NumChannels = 2;
+    
+    wh -> SampleRate = 44100;
+    wh -> BitsPerSample = 16;
+    
+    wh -> ByteRate = wh->NumChannels * wh -> SampleRate * wh -> BitsPerSample/8;
+    wh -> BlockAlign = wh->NumChannels * wh -> BitsPerSample/8;
+    
+    memcpy(wh->SubChunk2ID, &"data", 4);
+    return wh;
+}
+
+void setLengthForWaveFormatHeader(waveFormatHeader_t * wh, size_t numberOfFrames){
+    wh -> ChunkSize = (40 - 8) + wh -> Subchunk2Size; //header-8 + data size; header size is always 40 for stereo16bit44khz
+    wh -> Subchunk2Size = numberOfFrames * wh->NumChannels * wh->BitsPerSample/8;  
+}
+
+waveFormatHeader_t * stereo16bit44khzWaveHeaderForLength(size_t numberOfFrames){
+    waveFormatHeader_t * wh = stereo16bit44khzWaveHeader();
+    setLengthForWaveFormatHeader(wh, numberOfFrames);
+    return wh;
+}
+
+//TODO: write each member just in case your compiler pads any of the struct members, though it shouldn't (do to its layout.)
+size_t writeWaveHeaderToFile(waveFormatHeader_t * wh, FILE * file){
+    return fwrite(wh, sizeof(waveFormatHeader_t), 1, file); 
+}
